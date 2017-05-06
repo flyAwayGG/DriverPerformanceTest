d529ac28ede444e5d9ff18e440c069e09196b6f7
nikita <flo2motion@gmail.com>
2017-05-06 22:41:25 +0300

    d

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -28,24 +30,36 @@ public class Tester {
         };
     }
 
-    // This test will run 4 times since we have 5 parameters defined
     @Test(dataProvider = "Drivers")
     public void test(DriverProvider driverProvider, String driverName) {
-        WebDriver driver = driverProvider.getDriver();
+        WebDriver driver = timeCounterRet(driverProvider::getDriver);
 
-        System.out.println(parseDriverName(driver.toString()));
+//        timeCounter(()->driver.get("https://google.com/"));
+//        timeCounter(()->driver.findElement(By.cssSelector("")));
+
         driver.quit();
     }
 
+    @AfterClass
+    public void afterClass(){
+
+    }
+
+
+    private <T> T timeCounterRet(Supplier<T> toCalc) {
+        long startTime = System.currentTimeMillis();
+        T t = toCalc.get();
+        long endTime = System.currentTimeMillis()-startTime;
+        System.out.println(endTime/1000.0);
+        return t;
+    }
 
     private <T> void timeCounter(Supplier<T> toCalc) {
         long startTime = System.currentTimeMillis();
         toCalc.get();
-        long endTime = startTime - System.currentTimeMillis();
+        long endTime = System.currentTimeMillis()-startTime;
+        System.out.println(endTime/1000.0);
     }
 
-    private String parseDriverName(String driverToString) {
-        return driverToString.split(":")[0];
-    }
 
 }
